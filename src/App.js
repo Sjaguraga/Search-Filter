820017da0b12690ffd0c3bd7627a080085c0385f
serref jaguraga <reef163@gmail.com>
2022-06-22 00:53:18 -0400

    completed

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import JSONDATA from "./MOCK_DATA.json";
+import "./App.css";
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  console.log(searchTerm);
+  return (
+    <div className="App">
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(event) => {
+          setSearchTerm(event.target.value);
+        }}
+      />
+      {JSONDATA.filter((val) => {
+        if (searchTerm == "") {
+          return val;
+        } else if (
+          val.first_name.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+        ) {
+          return val;
+        }
+      }).map((val, key) => {
+        return (
+          <div className="user" key={key}>
+            <p>{val.first_name}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
